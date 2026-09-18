200e643de5f4decb411ac885728294743cf2970c
khanhduygithub <truongnguyenkhanhduy2010pr@gmail.com>
2026-09-19 01:53:39 +0700

    Delete darksword directory

--- darksword/esp/headers/UIEvent+Private.h
+++
@@ -1,7 +0,0 @@
-
-#import <UIKit/UIKit.h>
-
-@interface UIEvent (Private)
-- (void)_addTouch:(UITouch *)touch forDelayedDelivery:(BOOL)delayed;
-- (void)_clearTouches;
-@end
--- darksword/esp/sources/KIF/IOHIDEvent+KIF.h
+++
@@ -1,6 +0,0 @@
-
-#import <Foundation/Foundation.h>
-#import "IOKit+SPI.h"
-
-typedef struct __IOHIDEvent * IOHIDEventRef;
-IOHIDEventRef kif_IOHIDEventWithTouches(NSArray *touches) CF_RETURNS_RETAINED;
